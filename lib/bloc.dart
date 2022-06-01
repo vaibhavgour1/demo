483e260dcc11985ea70324312a423ec0af419752
vaibhav gour <vaibhav.gour@tekzee.com>
2022-06-01 16:50:01 +0530

    my first commite

--- lib/bloc.dart
+++ lib/bloc.dart
@@ -1,11 +1,9 @@
 import 'package:demo/data_model.dart';
-import 'package:demo/ervererror.dart';
 import 'package:demo/event.dart';
 import 'package:demo/main.dart';
 import 'package:demo/state.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:dio/src/response.dart';
-import 'package:dio/src/dio_error.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   HomeBloc() : super(InitialState()) {
@@ -13,27 +11,26 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
   }
 
   Future<TestResponse> getData(GetDataEvent event, Emitter<HomeState> emit) async {
-    emit(LoadingState());
-    try {
-   Response response = await dio.get(
-        "http://cgprojects.in/flutter/",
-      );
-   if(response.statusCode == 200){
-     emit(GetDataState(response: TestResponse.fromJson(response.toString())));
-   } else {
-     emit(FailureState(message: "Data not found"));
-   }
-      return TestResponse.fromJson(response.toString());
-    } catch (error) {
-      String message = "";
-      if (error is DioError) {
-        ServerError e = ServerError.withError(error: error);
-        message = e.getErrorMessage();
-      } else {
-        message = "Something Went wrong";
-      }
-      print("Exception occurred: $message");
-      return TestResponse(packages: []);
+    //try {
+    Response response = await dio.get(
+      "http://cgprojects.in/flutter/",
+    );
+    if (response.statusCode == 200) {
+      emit(GetDataState(response: TestResponse.fromJson(response.toString())));
+    } else {
+      emit(FailureState(message: "Data not found"));
     }
+    return TestResponse.fromJson(response.toString());
+    // } catch (error) {
+    //   String message = "";
+    //   if (error is DioError) {
+    //     ServerError e = ServerError.withError(error: error);
+    //     message = e.getErrorMessage();
+    //   } else {
+    //     message = "Something Went wrong";
+    //   }
+    //   print("Exception occurred: $message");
+    //   return TestResponse(packages: []);
+    // }
   }
 }
